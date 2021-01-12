2fca88263ad0035fbff88e0a9fc84759af1a5a9f
Noah <aminnnoah19@gmail.com>
2021-01-12 15:29:39 -0600

    changed api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: "http://localhost:8000/api",
- // API_ENDPOINT: "https://convo-to-go-server.herokuapp.com/api",
+ // API_ENDPOINT: "http://localhost:8000/api",
+  API_ENDPOINT: "https://convo-to-go-server.herokuapp.com/api",
   TOKEN_KEY: 'convo-to-go',
 }
